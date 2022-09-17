ca66a4bc25d59e12573c33ee80018456406266ec
casasDev <abramotivao@gmail.com>
2022-09-17 18:44:59 +0200

    Terrivial

--- app/src/androidTest/java/com/example/terrivial/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/terrivial/ExampleInstrumentedTest.kt
@@ -1,11 +1,8 @@
 package com.example.terrivial
-
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
