ec9417f54f1caba5624bdacf4507d1c122afe8c5
casasDev <abramotivao@gmail.com>
2022-09-18 18:59:35 +0200

    Cambio de activity

--- app/src/main/java/com/example/terrivial/vista/MainActivity.kt
+++ app/src/main/java/com/example/terrivial/vista/MainActivity.kt
@@ -1,28 +1,24 @@
 package com.example.terrivial.vista
-
-
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.terrivial.R
-import com.example.terrivial.modelo.Entretenimiento
 import com.example.terrivial.modelo.Partida
 
 class MainActivity : AppCompatActivity() {
     private val partida = Partida.getInstance()
-    private lateinit var jugar : Button
+    private lateinit var jugar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         partida.llenarMapas(this)
         jugar = findViewById(R.id.jugar)
-        partida.actualizarPregunta("Entretenimiento")
-        Log.d("OYEE", partida.preguntaActual.toString())
-        jugar.setOnClickListener{
-
+        jugar.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
         }
-
     }
+
+
 
 }
