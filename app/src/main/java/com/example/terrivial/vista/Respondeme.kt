0c611b438541487e99496cdbeaadbd476353ac63
casasDev <abramotivao@gmail.com>
2022-11-02 00:24:34 +0100

    Cambios ligeros

--- app/src/main/java/com/example/terrivial/vista/Respondeme.kt
+++ app/src/main/java/com/example/terrivial/vista/Respondeme.kt
@@ -1,14 +1,12 @@
 package com.example.terrivial.vista
 
 import android.annotation.SuppressLint
-import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
